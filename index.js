d2bdc76154de91a1e5db1553314b35ac6a0e9d35
andrigunawn <andri.gnwan@gmail.com>
2022-06-06 08:32:41 +0700

    sort array

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names,cbSort)=>{
+  let namesSorted = cbSort(names);
+  let newNames = [];
+  for (let i = 0; i < namesSorted.length; i++) {
+    const element =`${i+1}. ${namesSorted[i]}`;
+    newNames.push(element);
+        
+  }
+  return newNames;
+
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) =>{
+  return names.sort().reserve();
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
