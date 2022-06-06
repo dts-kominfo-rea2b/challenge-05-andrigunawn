a6a7cd0bfcaa3cf097c5b12b0b5ea266883bfe1b
andrigunawn <andri.gnwan@gmail.com>
2022-06-06 08:52:38 +0700

    edit sort function

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ const sorter = (names,cbSort)=>{
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
 const sortAscending = (names) => {
-  return names.sort(function(a, b){return a-b});
+  return names.sort();
 }
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
 const sortDescending = (names) =>{
-  return names.sort(function(a, b){return b-a});
+   names.sort();
+   return names.reverse();
 }
 
 // ! JANGAN DIMODIFIKASI
